1344f591c4e5bd3140e86f8de7ed5b8a617fb700
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-18 17:18:14 +1300

    add button beside words

--- client/components/Word.jsx
+++ client/components/Word.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-const Word = (props) => (
-  <div>{props.word}</div>
-)
+class Word extends React.Component {
+  constructor (props) {
+    super(props)
+    this.deleteWord = this.deleteWord.bind(this)
+  }
+ 
+  deleteWord(evt) {
+    console.log(evt.target.value)
+  }
+  render () {
+    return (
+      <div>
+      <p>{this.props.word}</p>
+      <button value={this.props.word} 
+        onClick={this.deleteWord}>
+        X
+      </button>
+    </div>
+    )
+  }
+}
 
 export default Word
