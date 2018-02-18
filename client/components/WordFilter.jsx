09800315cf195ad49bbfbd480dc031f0ef0d3324
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-18 19:16:15 +1300

    wordfilter comments

--- client/components/WordFilter.jsx
+++ client/components/WordFilter.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 import Word from './Word'
 
-import {filterWords} from '../actions'
-
 class WordFilter extends React.Component {
   constructor (props) {
     super(props)
@@ -12,10 +10,12 @@ class WordFilter extends React.Component {
     }
     this.filterWords = this.filterWords.bind(this)
   }
+  // look the current list of words and see if which ones include the value typed into the filter input
   filterWords(evt) {
     const filtering = this.props.words.filter(input => {
       return input.word.includes(evt.target.value)
     })
+    // set state to filtered words
     this.setState({
       filtered: filtering
     })
@@ -23,6 +23,7 @@ class WordFilter extends React.Component {
   render() {
     return (
       <div>
+        {/* pass filtered array as props to Word component */}
       <input onChange={this.filterWords} placeholder='filter words' />
       {this.state.filtered.map(words => {
         return (<Word key={`filtered${words.id}`} word={words.word} />)
@@ -32,7 +33,7 @@ class WordFilter extends React.Component {
   }
 }
 
-
+// map state to props so we can access original word list
 function mapStateToProps (state) {
   return {
     words: state.words
